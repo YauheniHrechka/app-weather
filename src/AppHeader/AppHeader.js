d04e2df90c9489f827bde920f1a11687f4a7c58b
Yauheni Hrechka <ip.grechko.ev@gmail.com>
2020-11-23 14:54:51 +0300

    Added the 'AppHeader' component

---
+++ src/AppHeader/AppHeader.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './AppHeader.css';
+
+class AppHeader extends Component {
+    render() {
+        return (
+            <h1>Header</h1>
+        )
+    }
+}
+
+export default AppHeader;
