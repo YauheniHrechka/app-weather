b07a1c89032cd5cd3642da7bf70c4768d280aad4
Yauheni Hrechka <ip.grechko.ev@gmail.com>
2020-11-23 14:59:44 +0300

    Added the 'AppMain' component

---
+++ src/AppMain/AppMain.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './AppMain.css';
+
+class AppMain extends Component {
+    render() {
+        return (
+            <h1>AppMain</h1>
+        )
+    }
+}
+
+export default AppMain;
